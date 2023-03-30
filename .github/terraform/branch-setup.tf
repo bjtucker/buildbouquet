87c0d156cb1bce9ebf4c9346bbd6b46ae4ffb366
Ben Tucker <bjtucker@users.noreply.github.com>
2023-03-30 03:16:56 -0500

    Update branch-setup.tf
    #3

--- .github/terraform/branch-setup.tf
+++ .github/terraform/branch-setup.tf
@@ -1,26 +1,15 @@
-provider "github" {}
-
-data "github_repository" "current" {
-  full_name = "bjtucker/buildbouquet"
-}
-
 resource "github_branch_protection" "main" {
   repository_id = data.github_repository.current.id
   pattern       = "main"
-
-  required_status_checks {
-    strict = true
-    contexts = [
-      "continuous-integration",
-    ]
-  }
 
   enforce_admins = true
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes = [
-      enforce_admins,
-    ]
   }
+
+  ignore_changes = [
+    enforce_admins,
+    required_pull_request_reviews,
+  ]
 }
