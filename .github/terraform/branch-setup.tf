b6cc99cb1df8acdfbab3c8f7e2bb235c5a11a346
Ben Tucker <bjtucker@users.noreply.github.com>
2023-03-30 03:04:19 -0500

    Update branch-setup.tf

--- .github/terraform/branch-setup.tf
+++ .github/terraform/branch-setup.tf
@@ -5,6 +5,10 @@ data "github_repository" "current" {
 }
 
 resource "github_branch_protection" "main" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
   repository_id = data.github_repository.current.id
   pattern       = "main"
 
