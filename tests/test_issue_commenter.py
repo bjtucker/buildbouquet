3fa20720913fc088336c7d34e7ab5ef51a06ebea
Ben Tucker <bjtucker@users.noreply.github.com>
2023-04-05 20:52:41 -0500

    Update test_issue_commenter.py

--- tests/test_issue_commenter.py
+++ tests/test_issue_commenter.py
@@ -1,5 +1,25 @@
-def test_prompt_formatting():
-    pass
+def test_create_prompt():
+    title = "Test issue title"
+    body = "Test issue body"
+    comments = ["Comment 1", "Comment 2"]
+    prompt = create_prompt(title, body, comments)
+    expected_prompt = f"""
+Here's an issue from the GitHub repository:
+
+Title: {title}
+
+Body: {body}
+
+Comments: {' '.join(comments)}
+
+Suggest a fix or refactoring task for this issue:
+
+Here are some Stack Overflow posts that may be helpful:
+
+{{}}
+"""
+    assert prompt == expected_prompt
+
 
 def test_response_generation():
     pass
